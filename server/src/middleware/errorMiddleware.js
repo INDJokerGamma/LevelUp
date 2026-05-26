4fe805f3a00b39c35222e8e356bdcf0c9eba56d4
INDJokerGamma <piyushdawkhare0000@gmail.com>
2026-05-26 13:32:37 +0530

    error Middle Ware  updated: added CastError, 11000 , validation Error

--- server/src/middleware/errorMiddleware.js
+++ server/src/middleware/errorMiddleware.js
@@ -6,5 +6,31 @@ const notfound = (req, res, next) =>{
 
 const errorHandler =( err, req, res, next)=>{
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
     
-}
+    if(err.name === "CastError"){
+        statusCode = 404;
+        message = "Resource not Found";
+    }
+
+    if(err.code === 11000){
+        statusCode = 409;
+        message = "Field Vlaue already Present";
+    }
+
+    if(err.code ==="ValidationError"){
+        statusCode = 400;
+        message = Object.values(err.errorrs).map((item) => item.message).join(", ");
+    }
+
+    res.status(statusCode).json({
+        success: false,
+        message,
+        stack: process.env.NODE_ENV === "production" ? null :err.stack,
+    });
+};
+
+module.export ={
+    notfound,
+    errorHandler,
+};
