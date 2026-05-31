c6724084ce186bfda2c6c4605fb2207f7ae26599
INDJokerGamma <piyushdawkhare0000@gmail.com>
2026-05-31 11:17:25 +0530

    add user model and token service for authentication

---
+++ server/src/modles/User.js
@@ -0,0 +1,234 @@
+const mongoose = require ('mongoose');
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        minlength:[2, "Name must be atleast 5 charcters"],
+        maxlength:[60, "Name must not exceed 60 Characters"],
+    },
+    username:{
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength:[5, "Username must be atleast 5 charcters"],
+        maxlength:[30, "Username must not exceed 30 Characters"],
+        match:[
+            /^[a-zA-Z0-9_]+$/,
+            "Username can only contain Letters, numbers and underScores",
+        ],
+    },
+
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            "Please enter a valid email"
+        ],
+    },
+
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+        minlength:[8,"Password must be atleast 8 charcters"],
+        select: false,
+    },
+
+    avatar:{
+        type: String,
+        default:" ",
+    },
+
+    bio:{
+        type: String,
+        default: " ",
+        maxlength: [300, "Bio cannot exceed more than 300 charcters"],
+    },
+
+    role:{
+        type: String,
+        enum:["user","admin"],
+        default:"user",
+    },
+
+    isEmailVerified:{
+        type: Boolean,
+        default: false,
+    },
+
+    level:{
+        type: Number,
+        default: 1,
+        min: 1,
+    },
+
+    xp:{
+        type:Number,
+        min:0,
+        default:0,
+    },
+
+    coins:{
+        type: Number,
+        min: 0,
+        default: 0,
+    },
+
+    gems:{
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+
+    rankTitle:{
+        type: String,
+        default: "beginner",
+    },
+
+    currentStreak:{
+        type: Number,
+        default: 0,
+        min:0,
+    },
+
+    longestSreak:{
+        type: Number,
+        default:0,
+        min:0,
+    },
+
+    totalHabitsComplete:{
+        type: Number,
+        default:0,
+        min:0,
+    },
+
+    focusMinutes:{
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+
+    productivityScore:{
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 100,
+    },
+
+    consistancyScore:{
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 0,
+    },
+
+    lifeBalanceScore:{
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 0,
+    },
+
+    unlockedThemes:{
+        type: [String],
+        default: ["Default"],
+    },
+
+    equippedTheme:{
+        type: String,
+        default: "default",
+    },
+
+    privacySetting:{
+        showProfile:{
+            type:Boolean,
+            default: true,
+        },
+        showstats:{
+            type: Boolean,
+            default: true,
+        },
+
+        showWellnessData:{
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    NotificationSetting:{
+        habitReminder:{
+            type: Boolean,
+            default: true,
+        },
+        questReminder:{
+            type: Boolean,
+            default: true,
+        },
+        StreakWarning:{
+            type: Boolean,
+            default: true,
+        },
+        Motivation:{
+            type: Boolean,
+            default: true,
+        },
+    },
+
+    passwordResetToken:{
+        type: String,
+        select: false,
+    },
+    passwordResetExpires:{
+        type: Date,
+        select: false,
+    },
+    emailVerificationToken:{
+        type:String,
+        select: false,
+    },   
+},
+{
+    timestamps: true,
+}
+);
+
+userSchema.index({username: 1});
+userSchema.index({email: 1});
+userSchema.index({xp:-1});
+userSchema.index({level: -1});
+userSchema.index({createdAt: -1});
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        next();
+        return ;
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.toSafeObject = function() {
+    const user = this.toObject();
+    delete user.password ;
+    delete user.passwordResetToken;
+    delete user.passwordResetExpires;
+    delete user.emailVerificationToken;
+    return user;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ server/src/services/tokenService.js
@@ -0,0 +1,35 @@
+const jwt = require("jasonwebtoken");
+
+const signToken = (userId) =>{
+    return jwt.sign({userId}, ProcessingInstruction.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    });
+};
+
+const sendTokenCookie = (res, token) =>{
+    const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
+        maxAge: 7*24*60*60*1000,
+    });
+};
+
+const clearTokenCookie = (res) =>{
+    const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("token", "", {
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
+        expires: new Date(0),
+    });
+};
+
+module.exports = {
+    signToken,
+    sendTokenCookie,
+    clearTokenCookie,
+};
