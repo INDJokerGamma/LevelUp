929e0141c2f3a6b7fc71585ce8a0379fe025d9ba
INDJokerGamma <piyushdawkhare0000@gmail.com>
2026-06-14 23:10:27 +0530

    authRotes js has been updated

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const{
+    signup,
+    login,
+    logout,
+    getMe,
+    forgotPassword,
+    resetPassword,
+} = require ("../controllers/authController");
+
+const {protect} = require ("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/me", protect, getMe);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token",resetPassword);
+
+module.export = router;
